6d1744e9f57c3e133acaf1d7c4483f65943612f1
Martin Ferretti <ferrettimartin@gmail.com>
2018-11-17 02:19:08 -0500

    Minor update

---
+++ build/CMakeFiles/lexy.desktop.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lexy.desktop"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/lexy.desktop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/po/CMakeFiles/lexy.alexa.pot.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lexy.alexa.pot"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/lexy.alexa.pot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
